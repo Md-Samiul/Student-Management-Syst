a1f8e0544f33a6fdca290a1de84815c8a5f7ae8e
Md-Samiul <41705391+Md-Samiul@users.noreply.github.com>
2018-07-26 02:56:16 +0600

    Add files via upload

---
+++ CourseTeacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities
+{
+    public class CourseTeacher
+    {
+        public int tID { get; set; }
+        public int cID { get; set; }
+    }
+}
---
+++ Registration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities
+{
+    public class Registration
+    {
+        public int regID { get; set; }
+        public int sID { get; set; }
+        public int c1ID { get; set; }
+        public int c2ID { get; set; }
+        public int c3ID { get; set; }
+    }
+}
